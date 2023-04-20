ef259f951fedb29265b75ddfec7ccad328417b69
Sachinmahajana <sachinmahajan782@gmail.com>
2023-04-20 14:28:42 +0530

    [Sachin] Adding UC4-CompareMethod

---
+++ LineComparison276/LineComparison.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineComparison276
+{
+    internal class LineComparison
+    {
+        public int x1, y1, x2, y2, x3, y3, x4, y4;
+        public double length, length2;
+        public void LineCompare()
+        {
+            Console.WriteLine("Enter the value of x1 and y1");
+            int x1 = Convert.ToInt32(Console.ReadLine());
+            int y1 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value x2 and y2");
+            int x2 = Convert.ToInt32(Console.ReadLine());
+            int y2 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x3 and y3");
+            int x3 = Convert.ToInt32(Console.ReadLine());
+            int y3 = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the value of x4 and y4");
+            int x4 = Convert.ToInt32(Console.ReadLine());
+            int y4 = Convert.ToInt32(Console.ReadLine());
+        }
+        public void LengthCalculate()
+        {
+            length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            length2 = Math.Sqrt((x4 - x3) ^ 2 + (y4 - y3) ^ 2);
+            Console.WriteLine("Length of line1 is:" + length);
+            Console.WriteLine("Length of line2 is:" + length2);
+        }
+        public void Ifequals()
+        {
+            if (length.Equals(length2))
+            {
+                Console.WriteLine("Two lines are equal");
+                if (length.CompareTo(length2) == 1)
+                    Console.WriteLine("Length of line1 is greater than lin2");
+            }
+            else if (length.CompareTo(length2) == -1)
+                Console.WriteLine("Length of line1 is less than line2");
+            else
+            {
+                Console.WriteLine("Two lines are not Equal");
+            }
+            Console.WriteLine("Length of both Lines are equal");
+            if (length.Equals(length2))
+            {
+                Console.WriteLine("Lines are equals");
+            }
+            else
+            {
+                Console.WriteLine("Lines are not Equal");
+            }
+        }
+    }
+}
--- LineComparison276/Program.cs
+++ LineComparison276/Program.cs
@@ -4,44 +4,50 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Line Comparison Problem");
-            //UC3-Compareline
-            Console.WriteLine("Enter the value of x1 and y1");
-            int x1 = Convert.ToInt32(Console.ReadLine());
-            int y1 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of x2 and y2");
-            int x2 = Convert.ToInt32(Console.ReadLine());
-            int y2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Welcome to Line Comparison Problem");
+            ////UC3-Compareline
+            //Console.WriteLine("Enter the value of x1 and y1");
+            //int x1 = Convert.ToInt32(Console.ReadLine());
+            //int y1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the value of x2 and y2");
+            //int x2 = Convert.ToInt32(Console.ReadLine());
+            //int y2 = Convert.ToInt32(Console.ReadLine());
 
-            double length = Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
-            Console.WriteLine("Line of length is: " + length);
+            //double length= Math.Sqrt((x2 - x1) ^ 2 + (y2 - y1) ^ 2);
+            //Console.WriteLine("Line of length is: " + length);
 
-            Console.WriteLine("Enter the value of x3 and y3");
-            int x3 = Convert.ToInt32(Console.ReadLine());
-            int y3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter the value of x4 and y4");
-            int x4 = Convert.ToInt32(Console.ReadLine());
-            int y4 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the value of x3 and y3");
+            //int x3 = Convert.ToInt32(Console.ReadLine());
+            //int y3 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Enter the value of x4 and y4");
+            //int x4 = Convert.ToInt32(Console.ReadLine());
+            //int y4 = Convert.ToInt32(Console.ReadLine());
 
-            double line2 = Math.Sqrt((x3 - y3) ^ 2 + (x4 - y4) ^ 2);
-            Console.WriteLine("Line of length is: " + line2);
+            //double length2 = Math.Sqrt((x3 - y3) ^ 2 + (x4 - y4) ^ 2);
+            //Console.WriteLine("Line of length is: " + length2);
 
-            if (length.Equals(line2))
-            {
-                Console.WriteLine("Both line length are equal");
-            }
-            else if (length.CompareTo(line2) == 1)
-            {
-                Console.WriteLine("Length of line is greater than line2 of line");
-            }
-            else if(length.CompareTo(line2) == -1)
-            {
-                Console.WriteLine("Length of line is less than line2 of line");
-            }
-            else
-            {
-                Console.WriteLine("Line length are not equal");
-            }
+            //if (length.Equals(length2))
+            //{
+            //    Console.WriteLine("Both line length are equal");
+            //}
+            //else if (length.CompareTo(length2) == 1)
+            //{
+            //    Console.WriteLine("Length of line is greater than line2 of line");
+            //}
+            //else if(length.CompareTo(length2) == -1)
+            //{
+            //    Console.WriteLine("Length of line is less than line2 of line");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("Line length are not equal");
+            //}
+
+            //OOps concept
+            LineComparison linecomparison = new LineComparison();
+            linecomparison.LineCompare();
+            linecomparison.LengthCalculate();
+            linecomparison.Ifequals();
         }
     }
 }
